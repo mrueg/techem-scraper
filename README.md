561daf4af99e258455fc70f3575641f9b12de59b
Manuel Rüger <manuel@rueg.eu>
2025-04-27 22:23:37 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,121 @@
 # techem-scraper
-Scrapes water/heating consumption data ("Verbrauchsinfo") from mieter.techem.de
+
+## Purpose
+Scrapes water/heating consumption data ("Verbrauchsinfo") from mieter.techem.de.
+
+## Background
+
+Techem offers a portal to show the "Verbrauchsinfo" to tenants to inform them about their energy consumption.
+The "Verbrauchsinfo" is a monthly generated usage report for heating and cooling as well as cold and warm water.
+
+If you live in a multi-tenant building, Techem might be the provider for water and heating meters. 
+
+> [!WARNING]
+> Unfortunately Techem does not offer an API client, so this is an experimental way of fetching it programmatically.
+> This project is not affiliated with Techem in any shape or form.
+> The backend might change any time, so use at your own risk.
+
+## How to use
+
+Have Python 3.x and the [MSAL](https://github.com/AzureAD/microsoft-authentication-library-for-python) python library installed.
+
+
+You need to be logged out of the mieter.techem.de portal in your browser (no active session), then run
+
+``` 
+./techem.py
+``` 
+
+## How it works
+
+The portal mieter.techem.de manages user logins via [Azure Active Directory B2C](https://azure.microsoft.com/en-us/products/active-directory-b2c).
+It uses an OAuth 2.0 Auth Code Flow, for which the scraper will open a web browser for you to log in with your credentials.
+
+After copying in the Redirect URL (starts with https://mieter.techem.de/auth?state), the script will automatically fetch your consumption data and create three types of files:
+
+* period.json (containing the periods you have stored)
+* YYYY-MM.json (containing the consumption data of the specific month)
+* YYYY-MM-average.json (containing the consumption of other comparably-sized appartments
+
+## Sample data
+
+The data provided by the API looks like this:
+
+```
+{
+    "count": 4,
+    "data": [
+        {
+            "period": "2023-01",
+            "service": "HOT_WATER",
+            "unitOfMeasure": "M3",
+            "amount": 1.9,
+            "quality": 1.0,
+            "revision": 2,
+            "status": "OK",
+            "createdAt": "2025-04-27T17:13:07Z"
+        },
+        {
+            "period": "2023-01",
+            "service": "HOT_WATER",
+            "unitOfMeasure": "KWH",
+            "amount": 223.0,
+            "quality": 1.0,
+            "revision": 2,
+            "status": "OK",
+            "createdAt": "2025-04-27T17:13:07Z"
+        },
+        {
+            "period": "2023-01",
+            "service": "HEATING",
+            "unitOfMeasure": "HCU",
+            "amount": 2504.0,
+            "quality": 1.0,
+            "revision": 2,
+            "status": "OK",
+            "createdAt": "2025-04-27T17:13:07Z"
+        },
+        {
+            "period": "2023-01",
+            "service": "HEATING",
+            "unitOfMeasure": "KWH",
+            "amount": 1912.9,
+            "quality": 1.0,
+            "revision": 2,
+            "status": "OK",
+            "createdAt": "2025-04-27T17:13:07Z"
+        }
+    ]
+}
+```
+
+The comparable data for average looks like:
+
+```
+{
+    "count": 2,
+    "revision": 2,
+    "data": [
+        {
+            "service": "HEATING",
+            "unitOfMeasure": "KWH",
+            "amount": 2392.92,
+            "createdAt": 1677926811000,
+            "status": "OK"
+        },
+        {
+            "service": "HOT_WATER",
+            "unitOfMeasure": "KWH",
+            "amount": 288.911507,
+            "createdAt": 1677926811000,
+            "status": "OK"
+        }
+    ]
+}
+```
+
+
+## Interesting findings about the backend
+
+* Blocks user agents like python requests
+* If the measurement for certain reasons is not plausible, the website won't show it. The API response shows the consumption with a status `EED_NE_BLACKLIST_IMPLAUSIBLE`.
---
+++ techem.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+# Fetches consumption data from https://mieter.techem.de/ programmatically
+
+import json
+import msal
+import webbrowser
+from urllib import parse
+import requests
+
+import logging
+
+AUTHORITY = "https://techemtenantportal.b2clogin.com/techemtenantportal.onmicrosoft.com/b2c_1a_signin"
+API_BASE = "https://mieter.techem.de/api/v1"
+CLIENT_ID = "e2c8cff8-17bc-41c7-89b6-5bee13c7f556"
+SCOPES = [
+    "https://techemtenantportal.onmicrosoft.com/eedo-be-consumption-service/access_as_user",
+]
+REDIRECT_URI = "https://mieter.techem.de/auth"
+
+LOG_LEVEL = logging.INFO
+
+
+def main():
+    log = logging.getLogger(__name__)
+    log.setLevel(LOG_LEVEL)
+    ch = logging.StreamHandler()
+    ch.setLevel(LOG_LEVEL)
+    log.addHandler(ch)
+    app = msal.PublicClientApplication(
+        client_id=CLIENT_ID, authority=AUTHORITY, client_credential=None
+    )
+    flow = app.initiate_auth_code_flow(scopes=SCOPES, redirect_uri=REDIRECT_URI)
+    if "error" in flow:
+        raise ValueError(flow.get("error"))
+    auth_url = flow["auth_uri"]
+    log.info(f"Go to this URL to login: {auth_url}")
+    log.info("A browser window should open automatically, if not enter the URL above")
+    webbrowser.open(auth_url, new=0, autoraise=True)
+    auth_response_url = input("Enter resulting redirect_url: ").rstrip("\n")
+    if not auth_response_url.startswith("https://mieter.techem.de/auth?state="):
+        raise Exception(
+            "Wrong URL, needs to start with https://mieter.techem.de/auth?state="
+        )
+    auth_response = dict(parse.parse_qsl(parse.urlsplit(auth_response_url).query))
+    result = app.acquire_token_by_auth_code_flow(flow, auth_response)
+    log.info("Auth successful")
+    log.debug(f"Auth Result:\n {result}")
+
+    rental_agreements = str.split(result["id_token_claims"]["rentalAgreements"][0], ";")
+    unit_id = rental_agreements[0]
+    party_id = rental_agreements[1]
+    base_url = f"{API_BASE}/consumptions/residential-units/{unit_id}/consumptions"
+    access_token = result["access_token"]
+    # python requests' UA gets blocked, so pretend to be Chrome
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "User-Agent": "Mozilla/5.0 (Windows NT 11.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.166 Safari/537.36",
+    }
+
+    # Fetch consumption periods
+    # https://mieter.techem.de/api/v1/consumptions/residential-units/UNIT_ID/consumptions/periods?limit=XX
+    periods_url = f"{base_url}/periods?limit=100"
+    log.debug(f"Fetching periods from: {periods_url}")
+    periods = requests.get(periods_url, headers=headers)
+    with open(f"periods.json", "w") as f:
+        json.dump(periods.json(), f, ensure_ascii=False, indent=4)
+
+    log.debug(f"Response body:\n{periods.text}")
+
+    data = periods.json()["data"]
+    log.info("Fetching consumption data")
+    for p in data:
+        period = p["period"]
+
+        # Fetch consumption data for this period
+        # https://mieter.techem.de/api/v1/consumptions/residential-units/UNIT_ID/consumptions/YYYY-MM
+        consumption_url = f"{base_url}/{period}"
+        log.debug(f"Fetching consumption data from: {consumption_url}")
+        consumption = requests.get(consumption_url, headers=headers)
+        with open(f"{period}.json", "w") as f:
+            json.dump(consumption.json(), f, ensure_ascii=False, indent=4)
+        log.debug(f"Response body:\n{consumption.text}")
+
+        # Fetch average data for this period (comparable to appartments similarly sized)
+        # https://mieter.techem.de/api/v1/consumptions/statistics/residential-units/UNIT_ID/consumptions/YYYY-MM/average
+        consumption_average_url = f"{API_BASE}/consumptions/statistics/residential-units/{unit_id}/consumptions/{period}/average"
+        log.debug(f"Fetching average consumption data from {consumption_average_url}")
+        consumption_average = requests.get(consumption_average_url, headers=headers)
+        with open(f"{period}-average.json", "w") as f:
+            json.dump(consumption_average.json(), f, ensure_ascii=False, indent=4)
+        log.debug(f"Response body:\n{consumption_average.text}")
+
+
+if __name__ == "__main__":
+    main()
